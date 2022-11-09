b728e5f208abe44a1f940c2ce1c8c239ffa435c2
Nikita Shalimo <MegaFrostBall@gmail.com>
2022-11-09 16:15:11 +0300

    add typed selector and actions custom hooks

--- src/components/pages/cart/Cart.tsx
+++ src/components/pages/cart/Cart.tsx
@@ -1,14 +1,17 @@
 import styles from "./Cart.module.scss";
 import { FC } from "react";
+import { useTypedSelector } from "../../../hooks/useTypedSelector";
+import { useActions } from "../../../hooks/useActions";
 import Layout from "../../ui/layout/Layout";
 
 const Cart: FC = () => {
+  const { products } = useTypedSelector((state) => state.cart);
 
   return (
     <Layout>
-        <h1>Cart</h1>
+      <h1>Cart</h1>
     </Layout>
-  )
-}
+  );
+};
 
 export default Cart;
