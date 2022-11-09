fb96fa2a4d8c375a5f2ab2c159f77eebfea5ddb0
Nikita Shalimo <MegaFrostBall@gmail.com>
2022-11-09 14:21:42 +0300

    add providers in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,26 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './components/pages/home/Home'
-import Product from './components/pages/product/Product'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
+import Home from "./components/pages/home/Home";
+import Product from "./components/pages/product/Product";
+import "./index.css";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/product/:id' element={<Product/>}/>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<Product />} />
+          </Routes>
+        </Router>
+      </PersistGate>
+    </Provider>
   </QueryClientProvider>
-)
+);
