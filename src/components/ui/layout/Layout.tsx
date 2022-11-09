e28411ac898ea90b21ca25e4ae9b8ba3cdca3826
Nikita Shalimo <MegaFrostBall@gmail.com>
2022-11-09 18:58:13 +0300

    add count products in cart and toggle title of cart in header

--- src/components/ui/layout/Layout.tsx
+++ src/components/ui/layout/Layout.tsx
@@ -1,29 +1,35 @@
-import { useQuery } from "@tanstack/react-query";
-import { Products } from "../../../services/products.service";
-import ProductCard from "../../ui/productCard/ProductCard";
-import styles from "./Layout.module.scss";
-import { FC, PropsWithChildren } from "react";
-import { Link } from "react-router-dom";
+import { useQuery } from '@tanstack/react-query'
+import { Products } from '../../../services/products.service'
+import ProductCard from '../../ui/productCard/ProductCard'
+import styles from './Layout.module.scss'
+import { FC, PropsWithChildren } from 'react'
+import { Link } from 'react-router-dom'
+import { useTypedSelector } from '../../../hooks/useTypedSelector'
 
 const Layout: FC<PropsWithChildren<{ title?: string }>> = ({
-  children,
-  title,
+	children,
+	title
 }) => {
-  const { data: products, isLoading } = useQuery(["products"], () =>
-    Products.getProducts()
-  );
+	const { products } = useTypedSelector(state => state.cart)
 
-  return (
-    <div className={styles.container}>
-      <header>
-        <nav>
-          <Link className={styles.link} to="/">Home</Link>
-          <Link className={styles.link} to="/cart">Cart</Link>
-        </nav>
-      </header>
-      {children}
-    </div>
-  );
-};
+	return (
+		<div className={styles.container}>
+			<header>
+				<nav>
+					<Link className={styles.link} to='/'>
+						Home
+					</Link>
+					<Link
+						className={products.length === 0 ? styles.link : styles.activeLink}
+						to='/cart'
+					>
+						Cart {products.length === 0 ? '' : `( ${products.length} )`}
+					</Link>
+				</nav>
+			</header>
+			{children}
+		</div>
+	)
+}
 
-export default Layout;
+export default Layout
